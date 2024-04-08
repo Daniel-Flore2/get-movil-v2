e38a5557bf7c29f63177d51eb5e7a41a19d8dbad
Daniel-Flore2 <danielflores.dev1@gmail.com>
2024-04-08 00:26:39 -0600

    feature post get-id-offer

--- src/store/User/infraestructure/offerRouter.ts
+++ src/store/User/infraestructure/offerRouter.ts
@@ -3,13 +3,22 @@ import { RegisterOfferUseCase } from '../appliaction/registerOfferUseCase';
 import { MysqlOfferRepository } from './mysqlOfferRepository';
 import express from 'express';
 import { GetofferController } from './controllers/getOfferController';
+import { GetOfferUseCase } from '../appliaction/getOfferUseCase';
+
+const offerRepository = new MysqlOfferRepository();
+const getOfferUseCase = new GetOfferUseCase(offerRepository);
+const getOfferController = new GetofferController(getOfferUseCase);
 
 export const offerRouter = express.Router();
 
-const offerRepository = new MysqlOfferRepository();
+
 const registerOfferUseCase = new RegisterOfferUseCase(offerRepository);
 const registerOfferController = new RegisterOfferController(registerOfferUseCase);
 
 
 // Ruta para registrar una nueva oferta
 offerRouter.post("/create-offer", registerOfferController.register.bind(registerOfferController));
+
+
+
+offerRouter.get("/:id", getOfferController.getByPublic.bind(getOfferController));
